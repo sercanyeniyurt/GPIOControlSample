be7a6460169b6b9014c4949a57b5271b7dd185e7
sercanyeniyurt <sercanyeniyurt@gmail.com>
2015-12-24 16:51:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import os
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-
+# The role and status information from being received
 role = int(sys.argv[1])
 islem = int(sys.argv[2])
 
-
+# If the number is greater than 0 GPIO make the setup role, implementing a process that you send.
 if role>0:
 			GPIO.setup(role,GPIO.OUT)
 			if islem>=0:
